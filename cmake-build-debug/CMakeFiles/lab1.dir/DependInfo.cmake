e4e4b22ff08e122858af88ca6f5d85c190f08f7d
Abdusalam <abdusalam.yab1996@gmail.com>
2020-11-06 21:07:51 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++/lab1/buggy_programs/bounds-heap.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/bounds-heap.cc.obj"
+  "D:/C++/lab1/buggy_programs/bounds.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/bounds.cc.obj"
+  "D:/C++/lab1/buggy_programs/dangling.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/dangling.cc.obj"
+  "D:/C++/lab1/buggy_programs/leak.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/leak.cc.obj"
+  "D:/C++/lab1/buggy_programs/sum.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/sum.cc.obj"
+  "D:/C++/lab1/buggy_programs/ub.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/buggy_programs/ub.cc.obj"
+  "D:/C++/lab1/cmake-example/main.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake-example/main.cc.obj"
+  "D:/C++/lab1/cmake-example/testlib/foo.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake-example/testlib/foo.cc.obj"
+  "D:/C++/lab1/coding.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/coding.cc.obj"
+  "D:/C++/lab1/editor.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/editor.cc.obj"
+  "D:/C++/lab1/file_io_example.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/file_io_example.cc.obj"
+  "D:/C++/lab1/print_argv.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/print_argv.cc.obj"
+  "D:/C++/lab1/separate_fn.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/separate_fn.cc.obj"
+  "D:/C++/lab1/separate_main.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/separate_main.cc.obj"
+  "D:/C++/lab1/test_coding.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test_coding.cc.obj"
+  "D:/C++/lab1/test_editor.cc" "D:/C++/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test_editor.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../cmake-example/testlib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/buggy_programs/bounds-heap.cc.obj"
+  "CMakeFiles/lab1.dir/buggy_programs/bounds.cc.obj"
+  "CMakeFiles/lab1.dir/buggy_programs/dangling.cc.obj"
+  "CMakeFiles/lab1.dir/buggy_programs/leak.cc.obj"
+  "CMakeFiles/lab1.dir/buggy_programs/sum.cc.obj"
+  "CMakeFiles/lab1.dir/buggy_programs/ub.cc.obj"
+  "CMakeFiles/lab1.dir/cmake-example/main.cc.obj"
+  "CMakeFiles/lab1.dir/cmake-example/testlib/foo.cc.obj"
+  "CMakeFiles/lab1.dir/coding.cc.obj"
+  "CMakeFiles/lab1.dir/editor.cc.obj"
+  "CMakeFiles/lab1.dir/file_io_example.cc.obj"
+  "CMakeFiles/lab1.dir/print_argv.cc.obj"
+  "CMakeFiles/lab1.dir/separate_fn.cc.obj"
+  "CMakeFiles/lab1.dir/separate_main.cc.obj"
+  "CMakeFiles/lab1.dir/test_coding.cc.obj"
+  "CMakeFiles/lab1.dir/test_editor.cc.obj"
+  "lab1.exe"
+  "lab1.exe.manifest"
+  "lab1.lib"
+  "lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
